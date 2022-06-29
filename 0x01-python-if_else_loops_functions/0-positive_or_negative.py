4cd971b655e3c640349b75c90288fca75d415782
Merc X <freddiecharlesjnr@gmail.com>
2022-06-29 05:02:37 +0100

    if py is best than C git commit, else do not commit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-    print("is positive\n")
+    print(f"{number} is positive\n")
 elif number < 0:
-    print("is negative\n")
+    print(f"{number} is negative\n")
 elif number == 0:
-    print("is zero")
+    print(f"{number} is zero\n")
 else:
-    print("number is not an integer")
+    print(f"{number} is not an integer\n")
