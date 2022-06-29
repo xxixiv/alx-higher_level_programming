44bbd86c019f16b1270bd2066e18325809175026
Merc X <freddiecharlesjnr@gmail.com>
2022-06-29 07:43:13 +0100

    alphas in bts

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    print("{}".format(chr(char)), end="")
+for a in range(97, 123):
+    print('{}'.format(chr(a)), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for a in range(97, 127):
+    if (a != 101, 113):  
+        print('{:c}'.format(a), end="")
